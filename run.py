d16082dfad28ceca0f0e2c503a99fcccc88933dc
arki <mrarkinpl@gmail.com>
2022-05-14 14:22:31 +0200

    #2

--- run.py
+++ run.py
@@ -18,88 +18,11 @@ def saveJson(filename, data):
 	with open(filename, "w") as outfile:
 		outfile.write(json_object)
 
-
-# goal = 10000
-
-# data = {
-# 	'day':[],
-# 	'month':{},
-# 	'allDays':[]
-# }
-
 def str_to_obj(string):
 	return datetime.strptime(string, '%d/%m/%y')
 
 def obj_to_str(object):
 	return object.strftime("%d/%m/%y")
-
-
-# today = datetime.now()
-# today2 = datetime.date(datetime.now())
-
-# today_str = today.strftime("%d/%m/%y")
-
-# date_time_obj = datetime.strptime(today_str, '%d/%m/%y')
-
-# print(date_time_obj)
-
-# d = date_time_obj - timedelta(days=1)
-
-# print(d)
-
-# skarbonka = False
-
-# while True:
-# 	print('1 - Dodaj do skarbonki\n2 - Next Day')
-# 	choice = input('.. ')
-# 	if choice == '1':
-# 		# skarbonka = True
-# 		# while skarbonka:
-# 		how = float(input('Ile chcesz dodac?\n'))
-# 		data['day'].append(how)
-# 	elif choice == '2':
-# 		day_income = sum(data['day'])
-# 		data['day'].clear()
-# 		data['allDays'].append(day_income)
-# 	elif choice == '3':
-# 		print(data)
-
-
-# if today_str in data:
-# 	data[today_str].append(5)
-# 	print('tak')
-# 	saveJson('modern bank\data.json',data)
-# else:
-# 	data[today_str] = []
-
-# 	saveJson('modern bank\data.json',data)
-
-
-
-# history = data['lastupdate']
-
-# history_obj = str_to_obj(history)
-
-# today = datetime.now()
-
-# delta = (today - history_obj).days
-
-# print(delta)
-
-# for x in range(int(delta)):
-# 	actual_obj = today - timedelta(days=x)
-# 	actual_str = obj_to_str(actual_obj)
-# 	if actual_str in data_all:
-# 		continue
-# 	else:
-# 		data_all[actual_str] = 0
-
-
-# print(data)
-
-
-# saveJson('modern bank\data.json',data)
-
 
 def checkStatus(file):
 	data = readJson(file)
@@ -121,7 +44,6 @@ def checkStatus(file):
 
 checkStatus('modern bank\data.json')
 saved = input('How much do you want to save?\n')
-
 
 try:
 	saved = float(saved)
